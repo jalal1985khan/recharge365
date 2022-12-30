e93974de2fc712e2e4d24a48121d09180c0d6232
jalal1985khan <jalal1985khan@gmail.com>
2022-12-31 00:05:44 +0530

    added MD

--- android/MobileRecharge/adduser.php
+++ android/MobileRecharge/adduser.php
@@ -23,8 +23,8 @@ date_default_timezone_set('Asia/Kolkata');
 	    $byID = $myID;
 	    
 	}
-$MDsql = "INSERT INTO masterdistributer (OWNER , ADMIN_ID , NAME , IMAGE, MOBILE , EMAIL , SMSBAL, RCBAL , DMRBAL, COMM, STATUS, ADDRESS, STATE, CITY, CUTTOFFAMOUNT, PASSWORD, REGDATE, COMM_PACK) VALUES ('$byStatus' , '$byID' , '$name' , 'default.jpeg' , '$mobile' , '$email', '0', '0', '0',  '0', 'Activate',  '$address', '$state' ,'$city','0' , '$pass_hash' , '$date','$packname') ";     
-
+//$MDsql = "INSERT INTO masterdistributer (OWNER , ADMIN_ID , NAME , IMAGE, MOBILE , EMAIL , SMSBAL, RCBAL , DMRBAL, COMM, STATUS, ADDRESS, STATE, CITY, CUTTOFFAMOUNT, PASSWORD, REGDATE, COMM_PACK) VALUES ('$byStatus' , '$byID' , '$name' , 'default.jpeg' , '$mobile' , '$email', '0', '0', '0',  '0', 'Activate',  '$address', '$state' ,'$city','0' , '$pass_hash' , '$date','$packname') ";     
+$MDsql = "INSERT INTO `masterdistributer`(`ID`, `OWNER`, `ADMIN_ID`, `IMAGE`, `NAME`, `MOBILE`, `EMAIL`, `MYKYC`, `SMSBAL`, `DMRBAL`, `RCBAL`, `COMM`, `STATUS`, `ADDRESS`, `STATE`, `CITY`, `CUTTOFFAMOUNT`, `COMM_PACK`, `PASSWORD`, `REGDATE`) VALUES ('','$byStatus','$byID','default.jpeg','$name','$mobile','$email','NA','0','0','0','0','Activate','$address','$state','$city','0','$packname','$pass_hash','$date')";
 	if ($con->query($MDsql) === TRUE) {
 		echo 'User Added';
 	  } else {
