c2071300bd855eaf234c65665091d8021d9631d6
jalal1985khan <jalal1985khan@gmail.com>
2022-12-31 02:46:21 +0530

    edit profile settings

--- android/CustomerCare.php
+++ android/CustomerCare.php
@@ -1,26 +1,18 @@
 <?php
  
- require_once('dbConnect.php');
+include('../includes/config.php');
  
-                 $mysql_qry = "SELECT * FROM `customer_supports`";
+        $mysql_qry = "SELECT * FROM `customer_supports`";
         $result = mysqli_query($con, $mysql_qry);
         $numbers_of_rows = mysqli_num_rows($result);
         $temp_array = array();
         if($numbers_of_rows > 0) {
-                    while ($row = mysqli_fetch_assoc($result)){
-                      
-                      
-                      
+          while ($row = mysqli_fetch_assoc($result)){
                 array_push($temp_array,array("id"=>$row["ID"],"name"=>$row["NAME"],"whatsapp"=>$row["WHATSAPP"],"whatsapptime"=>$row["WHATSAPP_TIME"],"mobile"=>$row["MOBILE"],"calltime"=>$row["CALL_TIME"],"position"=>$row['POSTION']));
-                
-                        
-                    }    
+                }    
                 echo json_encode($temp_array);        
-            
                 }
-                
                 else{
-                 
                  echo "No records found";
                     
                 }
