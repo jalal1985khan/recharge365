16549a06c3b95a4a18c9ded6f5e0496780047126
jalal1985khan <jalal1985khan@gmail.com>
2022-12-31 03:02:11 +0530

    edit profile settings

--- android/users/DetailsFetchUser.php
+++ android/users/DetailsFetchUser.php
@@ -1,25 +1,17 @@
 <?php
  
- require_once('dbConnect.php');
- 
+include("../../includes/config.php");
+$mobile = $_POST['MOBILE'];
+$password = $_POST['PASSWORD'];
+$password_sec = md5($password);
 
-
-     $mobile = $_POST['MOBILE'];
-     $password = $_POST['PASSWORD'];
-     
-    
-    
-    
-    $password_sec = md5($password);
-
-    
-                $mysql_qry = "SELECT * FROM `admin` WHERE MOBILE='".$mobile."' AND PASSWORD='".$password_sec."'";
-        $result = mysqli_query($con, $mysql_qry);
-        $numbers_of_rows = mysqli_num_rows($result);
-        $temp_array = array();
+$mysql_qry = "SELECT * FROM `admin` WHERE MOBILE='".$mobile."' AND PASSWORD='".$password_sec."'";
+$result = mysqli_query($con, $mysql_qry);
+$numbers_of_rows = mysqli_num_rows($result);
+$temp_array = array();
         if($numbers_of_rows > 0) {
                     while ($row = mysqli_fetch_assoc($result)){
-                        $imgpath = "https://www.recharges365.com/dashboard/admin/img/";
+                        $imgpath = "https://rch.hassantravels.in/images/";
                 array_push($temp_array,array("Name"=>$row["NAME"],"Image"=>$imgpath.$row["IMAGE"],"Owner"=>"Admin","OwnerId"=>"1","ID"=>$row["ID"],"Mobile"=>$row["MOBILE"],"RCBAL"=>$row["RCBAL"],"DMRBAL"=>$row["DMRBAL"],"SMSBAL"=>$row["SMSBAL"],"CUTTOFFAMOUNT"=>$row["CUTTOFFAMOUNT"],"COMM_PACK"=>$row["COMM_PACK"],"Status"=>$row["STATUS"],"APIACCESS"=>$row["APIACCESS"],"Email"=>$row["EMAIL"],"City"=>$row["CITY"],"Address"=>$row["ADDRESS"],"State"=>$row["STATE"],"Type"=>"admin","ImagePath"=>$imgpath));
             }    
                 echo json_encode($temp_array);        
