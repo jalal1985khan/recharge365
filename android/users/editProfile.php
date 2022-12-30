1d1363b65e60eb6ba621c2b61fc2af0dcb704114
jalal1985khan <jalal1985khan@gmail.com>
2022-12-31 02:15:09 +0530

    edit profile settings

--- android/users/editProfile.php
+++ android/users/editProfile.php
@@ -1,5 +1,24 @@
 <?php 
- echo "Update Succeed";
+    include("../../includes/config.php");
+    $name =  $_POST['name'];
+    $email = $_POST['email'];
+    $address= $_POST['address'];
+    $state = $_POST['state'];
+    $city = $_POST['city'];
+    $table = $_POST['table'];
+    $mobile = $_POST['mobile'];
+    $password = $_POST['password'];
+    $pass_hash = md5($password);
+    $check = "Unclear";
+
+    $details = $con->query("select * from `masterdistributer` where MOBILE='9731415095' and PASSWORD='b2fe7b1b2666db3112bfa165c22985f9'")->fetch_assoc();
+    $old_email = $details['EMAIL'];
+    echo $old_email;
+
+
+    $res = $con->query("SELECT * FROM masterdistributer order by NAME ASC");
+    echo $res;
+    echo "Update Succeed";
                                     
 
 
