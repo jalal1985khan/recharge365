e878fa4c45c4f17f296d0d55a4dac7cf7efb8f13
jalal1985khan <jalal1985khan@gmail.com>
2022-12-31 03:03:10 +0530

    edit profile settings

--- android/users/DetailsFetchUser.php
+++ android/users/DetailsFetchUser.php
@@ -33,7 +33,7 @@ $temp_array = array();
         if($numbers_of_rows > 0) {
                     while ($row = mysqli_fetch_assoc($result)){
                         
-                        $imgpath = "https://www.recharges365.com/dashboard/retailer/img/";
+                        $imgpath = "https://rch.hassantravels.in/images/";
                         
                         $Myowner = $row["OWNER"];
                             if($Myowner=="DISTRIBUTER"){
@@ -56,7 +56,7 @@ $temp_array = array();
         $temp_array =  array();
         if($numbers_of_rows > 0) {
                     while ($row = mysqli_fetch_assoc($result)){
-                        $imgpath = "https://www.recharges365.com/dashboard/masterdistributer/img/";
+                        $imgpath = "https://rch.hassantravels.in/images/";
                 array_push($temp_array,array("Name"=>$row["NAME"],"Image"=>$imgpath.$row["IMAGE"],"Owner"=>$row["OWNER"],"OwnerId"=>$row["ADMIN_ID"],"ID"=>$row["ID"],"Mobile"=>$row["MOBILE"],"RCBAL"=>$row["RCBAL"],"DMRBAL"=>$row["DMRBAL"],"SMSBAL"=>$row["SMSBAL"],"CUTTOFFAMOUNT"=>$row["CUTTOFFAMOUNT"],"COMM_PACK"=>$row["COMM_PACK"],"Status"=>$row["STATUS"],"APIACCESS"=>$row["APIACCESS"],"Email"=>$row["EMAIL"],"City"=>$row["CITY"],"Address"=>$row["ADDRESS"],"State"=>$row["STATE"],"Type"=>"masterdistributer","ImagePath"=>$imgpath));
             }    
                 echo json_encode($temp_array); 
@@ -71,7 +71,7 @@ $temp_array = array();
         $temp_array =  array();
         if($numbers_of_rows > 0) {
                     while ($row = mysqli_fetch_assoc($result)){
-                        $imgpath = "https://www.recharges365.com/dashboard/distributer/img/";
+                        $imgpath = "https://rch.hassantravels.in/images/";
                 array_push($temp_array,array("Name"=>$row["NAME"],"Image"=>$imgpath.$row["IMAGE"],"Owner"=>$row["OWNER"],"OwnerId"=>$row["MS_ID"],"ID"=>$row["ID"],"Mobile"=>$row["MOBILE"],"RCBAL"=>$row["RCBAL"],"DMRBAL"=>$row["DMRBAL"],"SMSBAL"=>$row["SMSBAL"],"CUTTOFFAMOUNT"=>$row["CUTTOFFAMOUNT"],"COMM_PACK"=>$row["COMM_PACK"],"Status"=>$row["STATUS"],"APIACCESS"=>$row["APIACCESS"],"Email"=>$row["EMAIL"],"City"=>$row["CITY"],"Address"=>$row["ADDRESS"],"State"=>$row["STATE"],"Type"=>"distributer","ImagePath"=>$imgpath));
             }    
                 echo json_encode($temp_array); 
@@ -86,7 +86,7 @@ $temp_array = array();
         $temp_array =  array();
         if($numbers_of_rows > 0) {
                     while ($row = mysqli_fetch_assoc($result)){
-                        $imgpath = "https://www.recharges365.com/dashboard/apiuser/img/";
+                        $imgpath = "https://rch.hassantravels.in/images/";
                         
                 array_push($temp_array,array("Name"=>$row["NAME"],"Image"=>$imgpath.$row["IMAGE"],"ID"=>$row["ID"],"Owner"=>"admin","OwnerId"=>"1","Mobile"=>$row["MOBILE"],"RCBAL"=>$row["RCBAL"],"DMRBAL"=>$row["DMRBAL"],"SMSBAL"=>$row["SMSBAL"],"COMM_PACK"=>$row["COMM_PACK"],"Status"=>$row["STATUS"],"Email"=>$row["EMAIL"],"City"=>$row["CITY"],"Address"=>$row["ADDRESS"],"State"=>$row["STATE"],"Type"=>"Api_users","ImagePath"=>$imgpath,"APIACCESS"=>$row["APIACCESS"],"CUTTOFFAMOUNT"=>$row["CUTTOFFAMOUNT"]));
             }    
