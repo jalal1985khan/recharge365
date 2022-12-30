7d060da0659322b53fb096d760c3c26a0f539b8a
jalal1985khan <jalal1985khan@gmail.com>
2022-12-31 03:28:53 +0530

    edit profile settings

--- android/users/changePassword.php
+++ android/users/changePassword.php
@@ -1,27 +1,16 @@
 <?php
-    include("config.php");
-    
+    include("../../includes/config.php");
     $table = $_POST['table'];
     $mobile = $_POST['mobile'];
     $old_pass = $_POST['password'];
     $new_pass = $_POST['newpassword'];
-    
-
-    
-    
     $old_pass_hash = md5($old_pass);
     $new_pass_hash = md5($new_pass);
     
             if(isset($new_pass)){
-            
-            
-            
-            mysqli_query($con,"UPDATE $table SET `PASSWORD`='$new_pass_hash' WHERE MOBILE = '$mobile' AND PASSWORD = '$old_pass_hash'");
-            
-            if(mysqli_affected_rows($con)>0){
-                
+            mysqli_query($con,"UPDATE $table SET `PASSWORD`='$new_pass_hash' WHERE MOBILE = '$mobile' AND PASSWORD = '$old_pass_hash'");  
+            if(mysqli_affected_rows($con)>0){    
                 echo "Password Successfully Changed";
-                
             }
 	       else
 	            {
@@ -29,8 +18,7 @@
 	            }
                 
             }
-            
-            
+
 	    else{
 	        
 	        echo "Password Couldn't Change";
