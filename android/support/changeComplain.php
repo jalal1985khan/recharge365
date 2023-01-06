a1f0b81aa7d51c7445d13e4ab9825330aff179df
jalal1985khan <jalal1985khan@gmail.com>
2023-01-07 00:20:08 +0530

    change complain 2

--- android/support/changeComplain.php
+++ android/support/changeComplain.php
@@ -1,60 +1,48 @@
 <?php
 
     include("../../includes/config.php");
-    $mobile = $_POST['mobile'];
-    $password = $_POST['password'];
+    // $mobile = $_POST['mobile'];
+    // $password = $_POST['password'];
+    // $txnID = $_POST['txnID'];
+    // $userMobile = $_POST['userMobile'];
+    // $statusGiven = $_POST['status'];
+    // $statusGiven = strtoupper($statusGiven);
+    // $remarks = $_POST['remarks'];
+    // $mn = $_POST['mn'];
+    // $pass_hash = md5($password);
+    // $date = date("Y-m-d");
+
+    $rechargeAmount = $_POST['rechargeAmount'];
+    $rechargeStatus = $_POST['rechargeStatus'];
+    $amountLeft = $_POST['amountLeft'];
+    $logo = $_POST['logo'];
+    $dateTime = $_POST['dateTime'];
     $txnID = $_POST['txnID'];
-    $userMobile = $_POST['userMobile'];
-    $statusGiven = $_POST['status'];
-    $statusGiven = strtoupper($statusGiven);
-    $remarks = $_POST['remarks'];
+    $operator = $_POST['operator'];
     $mn = $_POST['mn'];
-    $pass_hash = md5($password);
-    $date = date("Y-m-d");
+    $opID = $_POST['opID'];
+    $userType = $_POST['userType'];
+    $userNo = $_POST['userNo'];
+    $commAmount = $_POST['commAmount'];
+    $remarks = $_POST['remarks'];
 
-    if($txnID!=""){
-        
-        $mysql_qry = "SELECT * FROM `admin` WHERE MOBILE='".$mobile."' AND PASSWORD='".$pass_hash."'";
-        $result = mysqli_query($con, $mysql_qry);
-        $numbers_of_rows = mysqli_num_rows($result);
-        if($numbers_of_rows > 0)
-                    {
 
-                        
-                        $details = $con->query("SELECT * FROM 'rc_complaint' WHERE USER_NUMBER='$userMobile' AND TXN_ID='$txnID'")->fetch_assoc();
-                        $personTYPE = $details['USER_TYPE'];
-                        $amount = $details['RC_AMOUNT'];
-                        $comm_amount = $details['COMM_AMOUNT'];
-                        $rc_status = $details['RC_STATUS'];
-                        $rc_status = strtoupper($rc_status);
 
-                        if($statusGiven=="SUCCESS" && $rc_status=="FAILED"){
-                                echo 'SUCCESS';
-                        }
-                        else{
-                            echo 'FAILED';
-                        }
-                
-            
-                    
-                            
-                //  $delete  = $con->query("DELETE FROM `rc_complaint` WHERE USER_NUMBER='$userMobile' AND TXN_ID='$txnID'");
-                //     echo "Complain Deleted";
-            
+
+
+if($txnID!=""){
+
+$sql = "INSERT INTO `rc_complaint`(`RC_AMOUNT`, `RC_STATUS`, `AMOUNT_LEFT`, `LOGO`, `DATE_TIME`, `TXN_ID`, `OPERATOR`, `MN`, `OP_ID`, `USER_TYPE`, `USER_NUMBER`, `COMM_AMOUNT`, `REMARK`) VALUES ('$rechargeAmount','$rechargeStatus','$amountLeft','$logo','$dateTime','$txnID','$operator','$userNo','$opID','$userType','$mn','$commAmount','$remarks')";
+if(mysqli_query($con,$sql)){
+echo "SUCCESS";
+}
+    else{
+echo "SUCCESS";
+    }
       
-                            
-            
 
 
 
-            }
-                
-                else{
-                
-                    echo "FAILED";
-                    
-                }
-        
         
     }
     else{
