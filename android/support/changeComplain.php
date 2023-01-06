d935138d510f5e2d61d7cda679f11c5b4cf23a73
jalal1985khan <jalal1985khan@gmail.com>
2023-01-06 23:50:16 +0530

    change complain 2

--- android/support/changeComplain.php
+++ android/support/changeComplain.php
@@ -11,7 +11,8 @@
     $pass_hash = md5($password);
     $date = date("Y-m-d");
 
-    if($txnID!=""){
+    if($mn!=""){
+        ECHO 'SUCCESS';
         $mysql_qry = "SELECT * FROM `admin` WHERE MOBILE='".$mobile."' AND PASSWORD='".$pass_hash."'";
         $result = mysqli_query($con, $mysql_qry);
         $numbers_of_rows = mysqli_num_rows($result);
