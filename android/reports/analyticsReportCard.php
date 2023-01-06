702b50ee84c77795c073e2c5ad23ac86a57a5a1c
jalal1985khan <jalal1985khan@gmail.com>
2023-01-07 01:14:40 +0530

    change complain 2

--- android/reports/analyticsReportCard.php
+++ android/reports/analyticsReportCard.php
@@ -10,7 +10,7 @@
                         if($my_status=="admin"){
                                                         
                                                         
-                                                        $q = $con->query("SELECT * FROM recharge_history where DATE='$date' ORDER BY ID DESC");
+                                                        $q = $con->query("SELECT * FROM recharge_history  ORDER BY ID DESC");
                                                             
                                                             while($row = $q->fetch_assoc()){
 
