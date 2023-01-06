a7d4ae6d87a9bdbab9462c6a3cbf43b2c26af7a2
jalal1985khan <jalal1985khan@gmail.com>
2023-01-07 00:00:00 +0530

    change complain 2

--- android/support/changeComplain.php
+++ android/support/changeComplain.php
@@ -11,14 +11,23 @@
     $pass_hash = md5($password);
     $date = date("Y-m-d");
 
-    if($mn!=""){
+    if($txnID!=""){
         ECHO 'SUCCESS';
         $mysql_qry = "SELECT * FROM `admin` WHERE MOBILE='".$mobile."' AND PASSWORD='".$pass_hash."'";
         $result = mysqli_query($con, $mysql_qry);
         $numbers_of_rows = mysqli_num_rows($result);
         if($numbers_of_rows > 0)
                     {
-                      echo 'SUCCESS';
+                        $details = $con->query("SELECT * FROM 'rc_complaint' WHERE USER_NUMBER='$userMobile' AND TXN_ID='$txnID'")->fetch_assoc();
+                        $personTYPE = $details['USER_TYPE'];
+                        $amount = $details['RC_AMOUNT'];
+                        $comm_amount = $details['COMM_AMOUNT'];
+                        $rc_status = $details['RC_STATUS'];
+                        $rc_status = strtoupper($rc_status);
+
+                        if($statusGiven=="SUCCESS" && $rc_status=="PENDING"){
+                                echo 'SUCCESS';
+                        }
                 
             
                     
