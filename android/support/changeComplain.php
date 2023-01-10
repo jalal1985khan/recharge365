109199d6b0252b3c9dd277c082a4a7e6d2a9c81d
jalal1985khan <jalal1985khan@gmail.com>
2023-01-10 23:00:57 +0530

    changed complain file

--- android/support/changeComplain.php
+++ android/support/changeComplain.php
@@ -34,6 +34,7 @@ if($txnID!=""){
                     $rc_status = $details['RC_STATUS'];
                     $rc_status = strtoupper($rc_status);
                     $rc_id = $details['ID'];
+
                     if($statusGiven=="SUCCESS" && $rc_status=="FAILED"){
                     $q4 = $con->query("UPDATE `recharge_history` SET `STATUS`='$statusGiven' WHERE TRANS_ID='$txnID'");
                     if($q4){
@@ -67,22 +68,23 @@ if($txnID!=""){
                             $finalBal = $old_bal-$comm_amount;
                            $refund = $con->query("UPDATE `$personTYPE` SET `RCBAL`=$finalBal WHERE MOBILE='$userMobile'"); 
                             $q5 = $con->query("UPDATE `rc_complaint` SET `RC_STATUS`='$statusGiven',`REMARK`='RESOLVED',`COMPLAIN_STATUS`='RESOLVED' WHERE ID='$rc_id' AND TXN_ID='$txnID'");
-                            //$queryX1  = $con->query("INSERT INTO `amount_req`(`PERSON`, `TRANS_ID`, `USER`, `OWNER_ID`, `USER_ID`, `TYPE`, `PAYMENT_MODE`, `AMOUNT`, `FEE`, `STATUS`, `BEFORE_REQ`, `AFTER_REQ`, `TIME`) VALUES ('$owner','$txnID','$personTYPE','$ownerid','$user_id','Debit','Failed to Success','$comm_amount','0%','Deduction','$old_bal','$finalBal','$date')");
-                            if($q5){
-                                echo "SUCCESS";
-                            }
-                            
-
-                            
-                        
-
+                            $queryX1  = $con->query("INSERT INTO `amount_req`(`PERSON`, `TRANS_ID`, `USER`, `OWNER_ID`, `USER_ID`, `TYPE`, `PAYMENT_MODE`, `AMOUNT`, `FEE`, `STATUS`, `BEFORE_REQ`, `AFTER_REQ`, `TIME`) VALUES ('$owner','$txnID','$personTYPE','$ownerid','$user_id','Debit','Failed to Success','$comm_amount','0%','Deduction','$old_bal','$finalBal','$date')");
+                            echo "SUCCESS";
                     }
                     }
 // SUCCESS FAILED END HERE
+if($statusGiven=="SUCCESS" && $rc_status=="PENDING"){
+
+$q4 = $con->query("UPDATE `recharge_history` SET `STATUS`='$statusGiven' WHERE TRANS_ID='$txnID'");
+    if($q4){
+        $q5 = $con->query("UPDATE 'rc_complaint' SET 'COMPLAIN_STATUS'='RESOLVED','RC_STATUS'='$statusGiven','REMARK'='$remarks' WHERE ID='$rc_id' AND TXN_ID='$txnID'");
+        echo $statusGiven;
+    }
 
 
 
-
+}
+//---------------------------------------
                 }
     
 
