c79e423a6fcbb128d0f651ef88231e5e88e32866
jalal1985khan <jalal1985khan@gmail.com>
2023-01-12 23:24:10 +0530

    added breadcum

--- android/MobileRecharge/myROffer.php
+++ android/MobileRecharge/myROffer.php
@@ -45,7 +45,7 @@ if($test =='prepaid'){
 if($test=='dth')
 {
   $temp_array = array();
-  $live_url = "https://www.mplan.in/api/Dthinfo.php?apikey=26de55f672faa2f400bf5e1880448631&offer=roffer&tel=3025231060&operator=Airteldth";
+  $live_url = "https://www.mplan.in/api/Dthinfo.php?apikey=26de55f672faa2f400bf5e1880448631&offer=roffer&tel=$number&operator=$code";
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $live_url); //Using live here
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
