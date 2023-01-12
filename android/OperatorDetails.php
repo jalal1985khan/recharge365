2f418f4e5f933cd8b6389461e59dc78a8a2a23ac
jalal1985khan <jalal1985khan@gmail.com>
2023-01-13 02:06:21 +0530

    added breadcum

--- android/OperatorDetails.php
+++ android/OperatorDetails.php
@@ -2,8 +2,8 @@
  
  require_once('dbConnect.php');
  
-    $op_type = $_POST['op'];
-    // $op_type = "Broadband";
+    //$op_type = $_POST['op'];
+     $op_type = "Mobile";
  
    $mysql_qry = "SELECT * FROM `switchOperator` WHERE SERVICETYPE = '$op_type' and STATUS='Activate'";
     $result = mysqli_query($con, $mysql_qry);
